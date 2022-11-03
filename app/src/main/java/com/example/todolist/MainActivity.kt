e438e75adfe21c7e420390f32f09891da12da05d
ammarmuflih <49038472+ammarmuflih@users.noreply.github.com>
2022-11-03 15:37:19 +0700

    push test

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.todolist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.adapter.toDoListAdapter
 import com.example.todolist.database.databaseHelper
 import com.example.todolist.databinding.ActivityMainBinding
@@ -11,9 +13,11 @@ import com.example.todolist.model.todoListModel
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    var adapter : toDoListAdapter ?= null
+    lateinit var recycler_todo : RecyclerView
+    var todolistAdapter : toDoListAdapter ?= null
     var dbhandler : databaseHelper ?= null
     var todoList : List<todoListModel> = ArrayList<todoListModel>()
+    var linearlayoutManager : LinearLayoutManager ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         binding.addTodoButton.setOnClickListener{ toAddTodoActivity()}
 
         dbhandler = databaseHelper(this)
+        recycler_todo = findViewById(R.id.recyclerViewList)
     }
 
     private fun toAddTodoActivity(){
@@ -32,7 +37,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun fetchList(){
         todoList = dbhandler!!.getAllToDoList()
+        todolistAdapter = toDoListAdapter(todoList, applicationContext)
+        linearlayoutManager = LinearLayoutManager(applicationContext)
+        recycler_todo.layoutManager = linearlayoutManager
+        recycler_todo.adapter = todolistAdapter
+        todolistAdapter?.notifyDataSetChanged()
     }
-    
+
 }
 
